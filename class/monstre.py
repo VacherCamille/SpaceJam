ccacc79a28d367dee79d60bd4983a7488c91f66e
dodoiut2 <dorian.baret@etu.univ-grenoble-alpes.fr>
2019-02-04 17:21:41 +0100

    class monstre

---
+++ class/monstre.py
@@ -0,0 +1,7 @@
+class monstre:
+
+    def __init__(self):
+        self.pv = 100
+        self.speed = 10
+        self.degat = 1 #nombre de point qu'enlève le monstre au score du joueur
+        self.distance = 10
