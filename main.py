8e314b7e4509453db1085815f643c1ab4a847046
Dysymac <camille.vacher@etu.univ-grenoble-alpes.fr>
2019-02-04 16:49:15 +0100

    premier fichier

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from pygame.locals import *
 pygame.init()
 
 fenetre = pygame.display.set_mode((640,480),RESIZABLE)
-fond = pygame.image.load("background.jpg")
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond, (0,0))
 
+perso = pygame.image.load("perso.png").convert_alpha()
+fenetre.blit(perso, (200,300))
+pygame.display.flip()
 
 continuer = 1
 
