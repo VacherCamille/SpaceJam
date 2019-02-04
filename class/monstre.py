47750d8b592ebdffe505e3d63c100f0d76abb76b
dodoiut2 <dorian.baret@etu.univ-grenoble-alpes.fr>
2019-02-04 17:27:48 +0100

    class monstre.. ..

--- class/monstre.py
+++ class/monstre.py
@@ -1,7 +1,25 @@
-class monstre:
+class Monstre:
 
     def __init__(self):
         self.pv = 100
         self.speed = 10
         self.degat = 1  # nombre de point qu'enlève le monstre au score du joueur
         self.distance = 10
+
+
+class Pieuvre(Monstre):
+
+    def __init__(self):
+        Monstre.__init__(self)
+
+
+class Tireur(Monstre):
+
+    def __init__(self):
+        Monstre.__init__(self)
+
+
+class Coureur(Monstre):
+
+    def __init__(self):
+        Monstre.__init__(self)
