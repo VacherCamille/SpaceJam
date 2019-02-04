292b2ff6b88a697a06f2f0cfc354b1af2e40296e
Dysymac <camille.vacher@etu.univ-grenoble-alpes.fr>
2019-02-04 16:31:34 +0100

    premier fichier

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((640,480))
+fenetre = pygame.display.set_mode((640,480),RESIZABLE)
+fond = pygame.image.load("background.jpg")
 
 
+continuer = 1
+
+while continuer:
+    continuer = int(input())
 
 
 
