aea4623256eae30b7060d67cc3df753f534fd979
dodoiut2 <dorian.baret@etu.univ-grenoble-alpes.fr>
2019-02-06 21:09:07 +0100

    graph accueil

--- accueil.py
+++ accueil.py
@@ -10,24 +10,29 @@ fenetre.blit(fond, (0,0))
 
 pygame.display.set_caption('Menu Start')
 
+font = pygame.font.Font('American_Captain.ttf', 40)
 #affichage zone de text
 nameTxt = pygame.draw.rect(fenetre, (115, 194, 251), (387, 150, 250, 35))
 
 #affichage du bouton start
 start = pygame.image.load("bouton.png").convert()
 fenetre.blit(start ,  ( 390,200))
+fenetre.blit(font.render("start", True, (250, 128, 114)), (480, 235))
 
 #affichage du bouton crédits
 credits = pygame.image.load("bouton.png").convert()
 fenetre.blit(credits , ( 390,350))
+fenetre.blit(font.render("credits", True, (250, 128, 114)), (460, 385))
 
 #affichage du bouton highscore
 hscores = pygame.image.load("bouton.png").convert()
 fenetre.blit(hscores ,  (390, 500))
+fenetre.blit(font.render("hightscore", True, (250, 128, 114)), (440, 535))
 
 #affichage du bouton quiter
 quiter = pygame.image.load("bouton2.png").convert()
 fenetre.blit(quiter ,  (825, 650))
+fenetre.blit(font.render("quitter", True, (250, 128, 114)), (860, 670))
 
 pygame.display.flip()
 
