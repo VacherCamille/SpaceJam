3affbf218897973d988244ec290869c09f1ab164
dodoiut2 <dorian.baret@etu.univ-grenoble-alpes.fr>
2019-02-06 20:58:49 +0100

    orthographe flo

--- accueil.py
+++ accueil.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import *
 
 pygame.init()
 fenetre = pygame.display.set_mode((1024,768))
@@ -7,31 +8,26 @@ fenetre = pygame.display.set_mode((1024,768))
 fond = pygame.image.load("fond.png").convert()
 fenetre.blit(fond, (0,0))
 
-#coordonées start
-x_start = 300;
-y_start = 200;
-
-#coordonées crédits
-x_c = 300;
-y_c = 350;
-
 pygame.display.set_caption('Menu Start')
 
+#affichage zone de text
+nameTxt = pygame.draw.rect(fenetre, (115, 194, 251), (387, 150, 250, 35))
+
 #affichage du bouton start
-start = pygame.image.load("rectangle.png").convert()
-fenetre.blit(start ,  ( x_start,y_start))
+start = pygame.image.load("bouton.png").convert()
+fenetre.blit(start ,  ( 390,200))
 
 #affichage du bouton crédits
-credits = pygame.image.load("rectangle.png").convert()
-fenetre.blit(credits ,  ( x_c,y_c))
-
-#affichage du bouton quiter
-quiter = pygame.image.load("rectangle.png").convert()
-fenetre.blit(quiter ,  (900, 700))
+credits = pygame.image.load("bouton.png").convert()
+fenetre.blit(credits , ( 390,350))
 
 #affichage du bouton highscore
-hscores = pygame.image.load("rectangle.png").convert()
-fenetre.blit(hscores ,  (300, 500))
+hscores = pygame.image.load("bouton.png").convert()
+fenetre.blit(hscores ,  (390, 500))
+
+#affichage du bouton quiter
+quiter = pygame.image.load("bouton2.png").convert()
+fenetre.blit(quiter ,  (825, 650))
 
 pygame.display.flip()
 
@@ -41,7 +37,6 @@ while (running):
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-
             x, y = event.pos
             if start.get_rect(topleft=(300, 200)).collidepoint(x, y):
                 print('start')
@@ -56,6 +51,12 @@ while (running):
                 print(x, y)
 
             if quiter.get_rect(topleft=(900,700)).collidepoint(x, y):
+                print('quitter')
                 running = False
+
+            if nameTxt.collidepoint(x, y):
+                nbChar = 0
+                print("zone de text")
+
 #loop over, quite pygame
 pygame.quit()
