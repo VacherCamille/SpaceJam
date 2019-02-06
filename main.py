48fe387945941267d12b039d5c20f5057638a17d
CamilleVacher <camille.vacher26@gmail.com>
2019-02-06 10:28:54 +0100

    tests

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ width = 1024
 height = 768
 
 class Joueur(object):
-    def __init__(self,x,y,width,height,vel):
+    def __init__(self,x,y,width,height,vel,map):
         # # Sprites du personnage
         # self.droite = pygame.image.load(droite).convert_alpha()
         # self.gauche = pygame.image.load(gauche).convert_alpha()
@@ -31,7 +31,7 @@ class Joueur(object):
         self.height = height
         self.vel = vel
         # self.arme = Arme()
-        # self.vaisseau = Vaisseau()
+        self.map = map
         self.points = 0
         self.hitbox = (self.posx, self.posy, 50, 75)
 
@@ -51,6 +51,31 @@ class Projectil(object):
     def draw(self,fenetre):
         pygame.draw.circle(fenetre, self.color,(self.posx,self.posy), self.radius)
 
+class Monstre:
+    def __init__(self):
+        # # Sprites du monstre
+        # self.droite = pygame.image.load(droite).convert_alpha()
+        # self.gauche = pygame.image.load(gauche).convert_alpha()
+        # self.haut = pygame.image.load(haut).convert_alpha()
+        # self.bas = pygame.image.load(bas).convert_alpha()
+        self.pv = 100
+        self.speed = 10
+        self.degat = 1  # nombre de point qu'enlève le monstre au score du joueur
+        self.distance = 10
+        self.x = 0
+        self.y = 0
+        self.hitbox = (self.posx, self.posy, 50, 75)
+
+    def draw(self,fenetre):
+        fenetre.blit(perso, (self.x, self.y))
+        self.hitbox = (self.x, self.y, 50, 75)
+        pygame.draw.rect(fenetre, (255,0,0), self.hitbox,2)
+
+class Map(object):
+    def __init__(self,num):
+        self.num = num
+        self.meteor = []
+
 def redraw():
     fenetre.blit(fond, (0,0))
     hero.draw(fenetre)
@@ -58,7 +83,8 @@ def redraw():
         bullet.draw(fenetre)
     pygame.display.update()
 
-hero = Joueur(100,400,30,68,5)
+hero = Joueur(100,400,30,68,30,1)
+maps = [Map(0),Map(1),Map(2),Map(3),Map(4),Map(5),Map(6)]
 bullets =[]
 
 run = True
@@ -80,12 +106,23 @@ while run:
 
     if keys[pygame.K_LEFT] and hero.posx > 0:
         hero.posx -= hero.vel
+    elif keys[pygame.K_LEFT]:
+        hero.posx = 1024
+
     if keys[pygame.K_RIGHT] and hero.posx < 1024-30:
         hero.posx += hero.vel
-    if keys[pygame.K_DOWN]:
+    elif keys[pygame.K_RIGHT]:
+        hero.posx = 0
+    if keys[pygame.K_DOWN] and hero.posy < 768:
         hero.posy += hero.vel
-    if keys[pygame.K_UP]:
-        if len(bullets) < 5:
+    elif keys[pygame.K_DOWN]:
+        hero.posy = 0
+    if keys[pygame.K_UP] and hero.posy > 0:
+        hero.posy -= hero.vel
+    elif keys[pygame.K_UP]:
+        hero.posy = 768
+    if keys[pygame.K_SPACE]:
+        if len(bullets) < 25:
             bullets.append(Projectil(round(hero.posx + hero.width + 20 //2), round(hero.posy + hero.height//2), 6, (120,154,66),10))
             hero.posx -= 10
 
