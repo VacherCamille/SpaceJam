1181357999cc619bfdd980a3d689130390b14fa6
dodoiut2 <dorian.baret@etu.univ-grenoble-alpes.fr>
2019-02-06 22:16:58 +0100

    zone de texte

--- accueil.py
+++ accueil.py
@@ -4,39 +4,47 @@ from pygame import *
 pygame.init()
 fenetre = pygame.display.set_mode((1024,768))
 
-#image de fond
-fond = pygame.image.load("fond.png").convert()
-fenetre.blit(fond, (0,0))
+input_text = 'entrer votre nom'
 
-pygame.display.set_caption('Menu Start')
+def redraw():
+    global input_text, nameTxt, start, credits, hscores, quiter, font
 
-font = pygame.font.Font('American_Captain.ttf', 40)
-#affichage zone de text
-nameTxt = pygame.draw.rect(fenetre, (115, 194, 251), (387, 150, 250, 35))
+    #image de fond
+    fond = pygame.image.load("fond.png").convert()
+    fenetre.blit(fond, (0,0))
 
-#affichage du bouton start
-start = pygame.image.load("bouton.png").convert()
-fenetre.blit(start ,  ( 390,200))
-fenetre.blit(font.render("start", True, (250, 128, 114)), (480, 235))
+    pygame.display.set_caption('Menu Start')
 
-#affichage du bouton crédits
-credits = pygame.image.load("bouton.png").convert()
-fenetre.blit(credits , ( 390,350))
-fenetre.blit(font.render("credits", True, (250, 128, 114)), (460, 385))
+    font = pygame.font.Font('American_Captain.ttf', 40)
+    #affichage zone de text
+    nameTxt = pygame.draw.rect(fenetre, (115, 194, 251), (387, 150, 250, 35))
+    fenetre.blit(font.render(input_text, True, (0, 0, 0)), (390, 152))
 
-#affichage du bouton highscore
-hscores = pygame.image.load("bouton.png").convert()
-fenetre.blit(hscores ,  (390, 500))
-fenetre.blit(font.render("hightscore", True, (250, 128, 114)), (440, 535))
+    #affichage du bouton start
+    start = pygame.image.load("bouton.png").convert()
+    fenetre.blit(start ,  ( 390,200))
+    fenetre.blit(font.render("start", True, (250, 128, 114)), (480, 235))
 
-#affichage du bouton quiter
-quiter = pygame.image.load("bouton2.png").convert()
-fenetre.blit(quiter ,  (825, 650))
-fenetre.blit(font.render("quitter", True, (250, 128, 114)), (860, 670))
+    #affichage du bouton crédits
+    credits = pygame.image.load("bouton.png").convert()
+    fenetre.blit(credits , ( 390,350))
+    fenetre.blit(font.render("credits", True, (250, 128, 114)), (460, 385))
 
-pygame.display.flip()
+    #affichage du bouton highscore
+    hscores = pygame.image.load("bouton.png").convert()
+    fenetre.blit(hscores ,  (390, 500))
+    fenetre.blit(font.render("hightscore", True, (250, 128, 114)), (440, 535))
 
+    #affichage du bouton quiter
+    quiter = pygame.image.load("bouton2.png").convert()
+    fenetre.blit(quiter ,  (825, 650))
+    fenetre.blit(font.render("quitter", True, (0, 0, 0)), (860, 670))
+
+    pygame.display.flip()
+
+redraw()
 running = True
+clickEnterName = False
 while (running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,23 +53,25 @@ while (running):
             x, y = event.pos
             if start.get_rect(topleft=(300, 200)).collidepoint(x, y):
                 print('start')
-                print(x, y)
 
             if credits.get_rect(topleft=(300,350)).collidepoint(x, y):
                 print('crédits')
-                print(x, y)
 
             if hscores.get_rect(topleft=(300,500)).collidepoint(x, y):
                 print('hscores')
-                print(x, y)
 
             if quiter.get_rect(topleft=(900,700)).collidepoint(x, y):
                 print('quitter')
                 running = False
 
             if nameTxt.collidepoint(x, y):
+                clickEnterName = True
+                input_text = ''
+                redraw()
                 nbChar = 0
-                print("zone de text")
 
+        if event.type == pygame.KEYDOWN and clickEnterName and len(input_text)<10:
+            input_text = input_text + str(event.unicode)
+            redraw()
 #loop over, quite pygame
 pygame.quit()
