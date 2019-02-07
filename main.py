8448114b328bed8a9327991c3fbfb7d608a1f0ea
geitnert <geitnerteva@gmail.com>
2019-02-07 11:04:07 +0100

    df

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 def redraw():
     hero.map.draw(fenetre, imageVaisseau)
     hero.draw(fenetre)
-
+    hero.colision()
     fenetre.blit(text,(700,10))
     fenetre.blit(timerTxt,(512,10))
 
@@ -59,15 +59,21 @@ def initialisation_jeu():
     imageVaisseau = pygame.image.load("vaisseau.png")
     imageVaisseau = pygame.transform.scale(imageVaisseau, (300,200))
 
-    asteroides_map1 = [Asteroide(450, 350, 1)]
+    asteroides_map1 = [Asteroide(550, 500, 1), Asteroide(650, 150, 2), Asteroide(150, 550, 3), Asteroide(850, 50, 4), Asteroide(900, 650, 1),Asteroide(100, 100, 2)]
+    asteroides_map2 = [Asteroide(550, 500, 1), Asteroide(650, 150, 2), Asteroide(150, 550, 3), Asteroide(850, 50, 4), Asteroide(900, 650, 1),Asteroide(100, 100, 2)]
+    asteroides_map3 = [Asteroide(550, 500, 1), Asteroide(650, 150, 2), Asteroide(150, 550, 3), Asteroide(850, 50, 4), Asteroide(900, 650, 1),Asteroide(100, 100, 2)]
+    asteroides_map4 = [Asteroide(550, 500, 1), Asteroide(650, 150, 2), Asteroide(150, 550, 3), Asteroide(850, 50, 4), Asteroide(900, 650, 1),Asteroide(100, 100, 2)]
+    asteroides_map5 = [Asteroide(550, 500, 1), Asteroide(650, 150, 2), Asteroide(150, 550, 3), Asteroide(850, 50, 4), Asteroide(900, 650, 1),Asteroide(100, 100, 2)]
+    asteroides_map6 = [Asteroide(550, 500, 1), Asteroide(650, 150, 2), Asteroide(150, 550, 3), Asteroide(850, 50, 4), Asteroide(900, 650, 1),Asteroide(100, 100, 2)]
 
-    map0 = Map(0,"map.png",asteroides_map1)
+
+    map0 = Map(0,"map.png",[])
     map1 = Map(1,"map.png",asteroides_map1)
-    map2 = Map(2,"map.png",asteroides_map1)
-    map3 = Map(3,"map.png",asteroides_map1)
-    map4 = Map(4,"map.png",asteroides_map1)
-    map5 = Map(5,"map.png",asteroides_map1)
-    map6 = Map(6,"map.png",asteroides_map1)
+    map2 = Map(2,"map.png",asteroides_map2)
+    map3 = Map(3,"map.png",asteroides_map3)
+    map4 = Map(4,"map.png",asteroides_map4)
+    map5 = Map(5,"map.png",asteroides_map5)
+    map6 = Map(6,"map.png",asteroides_map6)
 
     vassal = Vaisseau("Aurora")
     hero = Joueur(100, 400, 30, 68, 7, map1, vassal)
@@ -82,83 +88,83 @@ def deplacement(hero):
     keys = pygame.key.get_pressed()
 
     # definition des changement de maps
-    if keys[pygame.K_LEFT] and hero.posx > 0:
-        hero.posx -= hero.vel
-        lastKey = "left"
-    elif keys[pygame.K_LEFT]:
-        lastKey = "left"
-        if hero.map.num == 1:
-            hero.map.num = 0
-            hero.posx = 1024
-        elif hero.map.num == 3:
-            hero.map.num = 2
-            hero.posx = 1024
-        elif hero.map.num == 4:
-            hero.map.num = 1
-            hero.posx = 1024
-        elif hero.map.num == 5:
-            hero.map.num = 6
-            hero.posx = 1024
+    # if keys[pygame.K_LEFT] and hero.posx > 0:
+    #     hero.mouvement_horizontal(-hero.vel)
+    #     lastKey = "left"
+    # elif keys[pygame.K_LEFT]:
+    #     lastKey = "left"
+    #     if hero.map.num == 1:
+    #         hero.map.num = 0
+    #         hero.posx = 1024
+    #     elif hero.map.num == 3:
+    #         hero.map.num = 2
+    #         hero.posx = 1024
+    #     elif hero.map.num == 4:
+    #         hero.map.num = 1
+    #         hero.posx = 1024
+    #     elif hero.map.num == 5:
+    #         hero.map.num = 6
+    #         hero.posx = 1024
+    #
+    # if keys[pygame.K_RIGHT] and hero.posx < 1024-50:
+    #     hero.mouvement_horizontal(hero.vel)
+    #     lastKey="right"
+    # elif keys[pygame.K_RIGHT]:
+    #     lastKey="right"
+    #     if hero.map.num == 0:
+    #         hero.map.num = 1
+    #         hero.posx = 0
+    #     elif hero.map.num == 1:
+    #         hero.map.num = 4
+    #         hero.posx = 0
+    #     elif hero.map.num == 2:
+    #         hero.map.num = 3
+    #         hero.posx = 0
+    #     elif hero.map.num == 6:
+    #         hero.map.num = 5
+    #         hero.posx = 0
+    #
+    # if keys[pygame.K_DOWN] and hero.posy < 768-  hero.height:
+    #     hero.mouvement_vertical(hero.vel)
+    #     lastKey = "down"
+    # elif keys[pygame.K_DOWN]:
+    #     lastKey = "down"
+    #     if hero.map.num == 1:
+    #         hero.map.num = 6
+    #         hero.posy = 0
+    #     elif hero.map.num == 2:
+    #         hero.map.num = 1
+    #         hero.posy = 0
+    #     elif hero.map.num == 3:
+    #         hero.map.num = 4
+    #         hero.posy = 0
+    #     elif hero.map.num == 4:
+    #         hero.map.num = 5
+    #         hero.posy = 0
+    #
+    # if keys[pygame.K_UP] and hero.posy > 0:
+    #     hero.mouvement_vertical(-hero.vel)
+    #     lastKey="up"
+    # elif keys[pygame.K_UP]:
+    #     lastKey="up"
+    #     if hero.map.num == 1:
+    #         hero.map.num = 2
+    #         hero.posy = 768
+    #     elif hero.map.num == 4:
+    #         hero.map.num = 3
+    #         hero.posy = 768
+    #     elif hero.map.num == 6:
+    #         hero.map.num = 1
+    #         hero.posy = 768
+    #     elif hero.map.num == 5:
+    #         hero.map.num = 4
+    #         hero.posy = 768
 
-    if keys[pygame.K_RIGHT] and hero.posx < 1024-50:
-        hero.posx += hero.vel
-        lastKey="right"
-    elif keys[pygame.K_RIGHT]:
-        lastKey="right"
-        if hero.map.num == 0:
-            hero.map.num = 1
-            hero.posx = 0
-        elif hero.map.num == 1:
-            hero.map.num = 4
-            hero.posx = 0
-        elif hero.map.num == 2:
-            hero.map.num = 3
-            hero.posx = 0
-        elif hero.map.num == 6:
-            hero.map.num = 5
-            hero.posx = 0
-
-    if keys[pygame.K_DOWN] and hero.posy < 768-  hero.height:
-        hero.posy += hero.vel
-        lastKey = "down"
-    elif keys[pygame.K_DOWN]:
-        lastKey = "down"
-        if hero.map.num == 1:
-            hero.map.num = 6
-            hero.posy = 0
-        elif hero.map.num == 2:
-            hero.map.num = 1
-            hero.posy = 0
-        elif hero.map.num == 3:
-            hero.map.num = 4
-            hero.posy = 0
-        elif hero.map.num == 4:
-            hero.map.num = 5
-            hero.posy = 0
-
-    if keys[pygame.K_UP] and hero.posy > 0:
-        hero.posy -= hero.vel
-        lastKey="up"
-    elif keys[pygame.K_UP]:
-        lastKey="up"
-        if hero.map.num == 1:
-            hero.map.num = 2
-            hero.posy = 768
-        elif hero.map.num == 4:
-            hero.map.num = 3
-            hero.posy = 768
-        elif hero.map.num == 6:
-            hero.map.num = 1
-            hero.posy = 768
-        elif hero.map.num == 5:
-            hero.map.num = 4
-            hero.posy = 768
-
-    if keys[pygame.K_SPACE]:
-        print(lastKey)
-        if len(bullets) < 25:
-            bullets.append(Projectil(round(hero.posx + hero.width + 20 //2), round(hero.posy + hero.height//2), 6, (120,154,66),45 , lastKey))
-            hero.recul(lastKey)
+    # if keys[pygame.K_SPACE]:
+    #     print(lastKey)
+    #     if len(bullets) < 25:
+    #         bullets.append(Projectil(round(hero.posx + hero.width + 20 //2), round(hero.posy + hero.height//2), 6, (120,154,66),45 , lastKey))
+    #         hero.recul(lastKey)
 
 
 def shoot(bullets):
@@ -234,7 +240,7 @@ def game():
 
         # definition des changement de maps
         if keys[pygame.K_LEFT] and hero.posx > 0:
-            hero.posx -= hero.vel
+            hero.mouvement_horizontal(-hero.vel)
             lastKey = "left"
         elif keys[pygame.K_LEFT]:
             lastKey = "left"
@@ -252,7 +258,7 @@ def game():
                 hero.posx = 1024
 
         if keys[pygame.K_RIGHT] and hero.posx < 1024 - 30:
-            hero.posx += hero.vel
+            hero.mouvement_horizontal(hero.vel)
             lastKey = "right"
         elif keys[pygame.K_RIGHT]:
             lastKey = "right"
@@ -270,7 +276,7 @@ def game():
                 hero.posx = 0
 
         if keys[pygame.K_DOWN] and hero.posy < 768 - hero.height:
-            hero.posy += hero.vel
+            hero.mouvement_vertical(hero.vel)
             lastKey = "down"
         elif keys[pygame.K_DOWN]:
             lastKey = "down"
@@ -288,7 +294,7 @@ def game():
                 hero.posy = 0
 
         if keys[pygame.K_UP] and hero.posy > 0:
-            hero.posy -= hero.vel
+            hero.mouvement_vertical(-hero.vel)
             lastKey = "up"
         elif keys[pygame.K_UP]:
             lastKey = "up"
